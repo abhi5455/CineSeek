7bca7c2f64a51c7c4b1f2e0f0c711814c38d2a1c
abhir <abhiramashok1062004@gmail.com>
2024-09-29 11:28:06 +0530

    Initial Commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,46 @@
+import './footer.css'
+import Card from "../card/Card.jsx";
+
+export default function Footer() {
+    return <div className='footer'>
+        <h2>Trending Now</h2>
+        <div className='cardContainer'>
+            <Card clsName={'card1'} imbdId={'tt15398776'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BMDBmYTZjNjUtN2M1MS00MTQ2LTk2ODgtNzc2M2QyZGE5NTVjXkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt0816692'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt12735488'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BMTM3ZGUwYTEtZTI5NS00ZmMyLTk2YmQtMWU4YjlhZTI3NjRjXkEyXkFqcGc@._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt11315808'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BNTRlNmU1NzEtODNkNC00ZGM3LWFmNzQtMjBlMWRiYTcyMGRhXkEyXkFqcGc@._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt8178634'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BODUwNDNjYzctODUxNy00ZTA2LWIyYTEtMDc5Y2E5ZjBmNTMzXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt6263850'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BZmQxZWM5MzgtY2EzZC00OGUxLWE0Y2EtMDIwOTFlNmQ5MWMyXkEyXkFqcGc@._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt16366836'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BZDMyYWU4NzItZDY0MC00ODE2LTkyYTMtMzNkNDdmYmFhZDg0XkEyXkFqcGc@._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt1877830'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BOGE2NWUwMDItMjA4Yi00N2Y3LWJjMzEtMDJjZTMzZTdlZGE5XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt25433734'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BMjZkNTdhOTMtYzg0Yy00YjA4LTljOTQtZWRkNTZkMTY3Y2Y3XkEyXkFqcGdeQXVyMTUzNTgzNzM0._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt1340094'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BMjEyOWJjMWQtZTY4Yi00ZjYyLWJhNDktZmRkNzEwMjlhZjQ3XkEyXkFqcGc@._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt2248028'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BMGJkYTczYmQtMWU4Zi00MDMyLThiY2MtNDVjNzk5NjY2YzY2XkEyXkFqcGdeQXVyODc5ODU4NDQ@._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt1375666'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt15980138'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BYWRhMWRjNzEtYmJkYi00NzcwLTk1YWYtYzk4NzRiOTlhODMzXkEyXkFqcGdeQXVyMjkxNzQ1NDI@._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt11531182'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BNjllMGI0NjMtZmMxOC00NTViLThmMGItNGIyN2QyMzFlZTAyXkEyXkFqcGdeQXVyMjkxNzQ1NDI@._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt27487934'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BNDQzYWNiZjEtY2MzNy00MDdmLWI1NmItY2RkN2IzM2FlN2MwXkEyXkFqcGdeQXVyMTUzNTgzNzM0._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt2049403'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BYmRlYzE3ZTYtNjIyZi00MWYyLWEzZDItOWI5NzM1Yjc0NmI5XkEyXkFqcGc@._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt17279496'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BYTYyODhlODktYjUzNC00NjUyLWI1MzYtNmI0MTY3YTUxYjY2XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg"}></Card>
+            <Card clsName={'card1'} imbdId={'tt1155076'} path={'/movie'}
+                  sourceLink={"https://m.media-amazon.com/images/M/MV5BYjQ1NzRhYjYtMWY3My00ODA0LTk5ZDctYjQ4YjE0M2RhMGNiXkEyXkFqcGdeQXVyNTUyMzE4Mzg@._V1_SX300.jpg"}></Card>
+        </div>
+    </div>
+}
